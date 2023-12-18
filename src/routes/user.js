097ead06dc18ecb4d45cfa5810a3795f957e999b
KwaneleNdaba <kwanelendaba113@gmail.com>
2023-12-18 22:53:35 +0200

    user update endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -20,7 +20,35 @@ router.get("/" , async (req,res) => {
         if(!users) return res.status(404).json({message: "Users not found"});
         return res.status(200).json(users);
     } catch (error) {
-        return res.status(500).jsons({message: error.message})
+        return res.status(500).json({message: error.message})
+    }
+})
+
+router.put("/:id", async (req,res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findByIdAndUpdate({_id : userId}, req.body, {new : true});
+
+        if(!user){
+            return res.status(404).json({message : "User not found"})
+        }
+
+        return res.status.json(user)
+    } catch (error) {
+        return res.status(500).json({message : error.message})
+    }
+
+})
+
+router.get("/:id", async (req,res) => {
+    try {
+        const user = await User.findById({_id : req.params.id})
+        if(!user){
+            return res.status(404).json({message : "User not found"})
+        }
+    } catch (error) {
+        return res.status(500).json({message : error.message})
     }
 })
 
